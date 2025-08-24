80d382eb90bf5f39e5e385865ee11677d702be43
Trashbin4943 <anthonyhwan415@gmail.com>
2025-08-25 00:18:21 +0900

    cors acceptance edit:

--- ms_hack/ms_hack/settings.py
+++ ms_hack/ms_hack/settings.py
@@ -25,6 +25,7 @@ CSRF_TRUSTED_ORIGINS = ['https://www.openddm.store', 'https://openddm.store']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
     "https://www.openddm.store",
     "https://openddm.store",
 ]
